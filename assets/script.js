53c5730776728103a97e95d47c04c6909bd953c2
Ryan <ryanpjhickey@gmail.com>
2022-08-01 16:30:55 -0700

    add question 1 and first answer option for question 1 along with wrong answer pop up

--- assets/script.js
+++ assets/script.js
@@ -48,6 +48,7 @@ var hide2 = document.querySelector('#hide2');
 var counter = 61;
 
 startQuiz.addEventListener('click', setCounterText);
+startQuiz.addEventListener('click', addQ1)
 
 timerA.innerHTML = "60";
 
@@ -55,12 +56,37 @@ function setCounterText() {
   console.log(counter);
   counter--;
   timerA.innerHTML = counter;
-  startQuiz.style.display = 'none';
-  hide.style.display = 'none';
-  hide2.style.display = 'none';
   if(counter === 60){
-    var countdown = setInterval(setCounterText, 1000);
+    countdown = setInterval(setCounterText, 1000);
   } else if(counter <= -1){
       timerA.innerHTML = "0"
     }
   };
+
+
+  var a1q1 = document.querySelector('#q1a1')
+  a1q1.style.display = "none"
+
+  function addQ1() {
+    startQuiz.style.display = 'none';
+    hide.style.display = 'none';
+    hide2.style.display = 'none';
+    var q1add = document.querySelector('#q1');
+    var q1 = document.createElement("p");
+    q1.textContent = "Commonly used datatypes do NOT include:";
+    q1add.appendChild(q1);
+    var q1a1 = document.querySelector('#q1a1');
+    var q1ans1 = document.createElement("div");
+    q1ans1.textContent = "1. strings";
+    q1a1.appendChild(q1ans1);
+    q1a1.style.display = 'inline';
+    a1q1.addEventListener('click', s1a1)
+  }
+
+  function s1a1() {
+    var incorrect = document.querySelector('#wrong');
+    var wrong = document.createElement('p');
+    wrong.textContent = 'Wrong!';
+    incorrect.appendChild(wrong);
+    incorrect.style.display ='inline'
+  }
