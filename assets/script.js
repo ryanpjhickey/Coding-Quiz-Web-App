bf0e0e38c97c65424bff04e48acd6d8f5877dd52
Ryan <ryanpjhickey@gmail.com>
2022-08-01 12:15:06 -0700

    test different types of code for timer

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,40 @@
-var counter = 60
-var countdown = function(){
-  counter--;
-  if(counter === 0) {
-    clearInterval(strtcd);
-  };
-};
+// var counter = 60
+//function countdown(){
+//  counter--;
+//  if(counter === 0) {
+//    clearInterval(strtcd);
+//  };
+//  return counter
+//};
 
-var strtcd = setInterval(countdown, 1000);
+//var strtcd = setInterval(countdown, 1000);
 
-strt.addEventListener('click', countdown)
+//document.querySelector('#start').addEventListener('click', countdown)
+
+//function strttimer() {
+//  var strt1 = countdown();
+//  document.querySelector('#timeleft').value = strt1;
+//}
+
+//var generateBtn = document.querySelector('#generate');
+
+//function generatePassword(event) {
+//  var counter = 60
+//  counter--;
+//  if(counter === 0) {
+//    console.log("blastoff");
+//    clearInterval(start);
+//  }
+//};
+
+//var start = setInterval(generatePassword, 1000)
+
+//function writePassword() {
+//  var password = generatePassword();
+//  document.querySelector('#password').value = password;
+//}
+
+// Add event listener to generate button
+//generateBtn.addEventListener('click', writePassword);
+
+var timerA = document.querySelector('#timer')
