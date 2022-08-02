72aabc2cadf3ffa6c790dc1b67b45c98ae2f6b51
Ryan <ryanpjhickey@gmail.com>
2022-08-02 11:43:48 -0700

    add ans 2-4 and add padding between answers

--- assets/script.js
+++ assets/script.js
@@ -26,8 +26,19 @@ function setCounterText() {
   };
 
 
-  var a1q1 = document.querySelector('#q1a1')
+  var a1q1 = document.querySelector('#a1q1')
   a1q1.style.display = "none"
+  a2q1.style.display = "none"
+  a3q1.style.display = "none"
+  a4q1.style.display = "none"
+  a1q2.style.display = "none"
+  a2q2.style.display = "none"
+  a3q2.style.display = "none"
+  a4q2.style.display = "none"
+  a1q3.style.display = "none"
+  a2q3.style.display = "none"
+  a3q3.style.display = "none"
+  a4q3.style.display = "none"
 
   function addQ1() {
     startQuiz.style.display = 'none';
@@ -37,12 +48,30 @@ function setCounterText() {
     var q1 = document.createElement("p");
     q1.textContent = "Commonly used datatypes do NOT include:";
     q1add.appendChild(q1);
-    var q1a1 = document.querySelector('#q1a1');
+    var q1a1 = document.querySelector('#a1q1');
     var q1ans1 = document.createElement("div");
     q1ans1.textContent = "1. strings";
     q1a1.appendChild(q1ans1);
     q1a1.style.display = 'inline';
     a1q1.addEventListener('click', s1a1)
+    var q1a2 = document.querySelector('#a2q1');
+    var q1ans2 = document.createElement("div");
+    q1ans2.textContent = "2. booleans";
+    q1a2.appendChild(q1ans2);
+    q1a2.style.display = 'inline';
+    a2q1.addEventListener('click', s1a1)
+    var q1a3 = document.querySelector('#a3q1');
+    var q1ans3 = document.createElement("div");
+    q1ans3.textContent = "3. cows";
+    q1a3.appendChild(q1ans3);
+    q1a3.style.display = 'inline';
+    a3q1.addEventListener('click', s1a1)
+    var q1a4 = document.querySelector('#a4q1');
+    var q1ans4 = document.createElement("div");
+    q1ans4.textContent = "4. JQuery";
+    q1a4.appendChild(q1ans4);
+    q1a4.style.display = 'inline';
+    a4q1.addEventListener('click', s1a1)
   }
 
   function s1a1() {
@@ -51,4 +80,18 @@ function setCounterText() {
     wrong.textContent = 'Wrong!';
     incorrect.appendChild(wrong);
     incorrect.style.display ='inline'
+    counter = counter - 10;
+  }
+
+  function addQ2() {
+    var q2add = document.querySelector('#q2');
+    var q2 = document.createElement("p");
+    q2.textContent = "Q2 placeholder";
+    q2add.appendChild(q2);
+    var q2a1 = document.querySelector('#a1q2');
+    var q2ans1 = document.createElement("div");
+    q2ans1.textContent = "1. strings";
+    q2a1.appendChild(q1ans1);
+    q2a1.style.display = 'inline';
+    a1q2.addEventListener('click', s1a1)
   }
